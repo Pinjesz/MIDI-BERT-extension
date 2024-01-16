231f05672307dab0eceb8d65498719ea982dac09
pinjesz_komputer <jasiekjeschke@gmail.com>
2024-01-16 11:02:52 +0100

    typo fix

--- scripts/cp/finetune.sh
+++ scripts/cp/finetune.sh
@@ -4,7 +4,7 @@ export PYTHONPATH="."
 python3 MidiBERT/cp/finetune.py --tokenization=cp --task=melody --name=default --num_workers=4 --batch_size=4
 
 # velocity
-python3 MidiBERT/cp/finetune.py --tokenization=cp --task=velociy --name=default --num_workers=4 --batch_size=4
+python3 MidiBERT/cp/finetune.py --tokenization=cp --task=velocity --name=default --num_workers=4 --batch_size=4
 
 # composer
 python3 MidiBERT/cp/finetune.py --tokenization=cp --task=composer --name=default --num_workers=4 --batch_size=4
